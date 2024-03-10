a9ea9e47148d794ec6eab34a9813e6d0f0fc180a
Filippo Orru <filippoorru@hotmail.com>
2024-03-10 20:28:22 +0100

    Calendar looks decent

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
-import { add, getDayOfYear, isSameDay, previousMonday } from "date-fns";
+import { add, subMilliseconds, getDayOfYear, isSameDay, previousMonday, isToday } from "date-fns";
 
-let today = new Date();
-let startOfTheWeek = previousMonday(today);
+let now = useState('today', () => new Date());
+function updateToday() {
+    now.value = new Date();
+}
+let updateTodayTimer: NodeJS.Timeout;
+
+onBeforeMount(() => {
+    updateTodayTimer = setInterval(updateToday, 1000);
+});
+onUnmounted(() => {
+    clearInterval(updateTodayTimer);
+});
+
+let startOfTheWeek = previousMonday(now.value);
+startOfTheWeek.setHours(0, 0, 0, 0);
 let endOfTheWeek = add(startOfTheWeek, { days: 6 });
+
+const oneDayMs = 24 * 60 * 60 * 1000;
+
+const scrollViewportRef = ref<HTMLDivElement>();
+const viewportIsReady = useState('viewportIsReady', () => false);
+
 let daysOfTheWeek = [
     "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
 ];
@@ -12,16 +31,24 @@ let thisWeek = daysOfTheWeek.map((day, index) => {
     return {
         dayOfTheWeek: day,
         date: date,
-        isToday: isSameDay(today, date)
+        isToday: isSameDay(now.value, date)
     }
 });
 
+function dateTimeMinutes(date: Date) {
+    return date.getHours() * 60 + date.getMinutes() + date.getSeconds() / 60;
+}
+
 function dateToPercentOfDay(date: Date) {
-    return (date.getHours() * 60 + date.getMinutes()) / (24 * 60) * 100;
+    return dateTimeMinutes(date) / (24 * 0.6);
 }
 
 function formatTime(date: Date) {
-    return date.getHours().toString().padStart(2, '0') + ":" + date.getMinutes().toString().padStart(2, '0');
+    return date.getHours().toString() + ":" + date.getMinutes().toString().padStart(2, '0');
+}
+
+function dateIsBetween(date: Date, start: Date, end: Date) {
+    return date.getTime() >= start.getTime() && date.getTime() <= end.getTime();
 }
 
 type CalendarEntry = {
@@ -30,14 +57,46 @@ type CalendarEntry = {
     title: string;
 };
 
-const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendarEntries', () => {
-    return [...Array(5 + Math.floor(Math.random() * 5))].map((_, index) => {
-        let start = add(startOfTheWeek, { hours: Math.random() * 7 * 24 });
+function eventDuration(event: CalendarEntry) {
+    return event.end.getTime() - event.start.getTime();
+}
+
+function eventIsShort(event: CalendarEntry) {
+    return eventDuration(event) < 40 * 60 * 1000;
+}
+
+const calendarEventsWithOverlapGroups: globalThis.Ref<Array<Array<CalendarEntry>>> = useState('calendarEntries', () => {
+    let events = [...Array(5 + Math.floor(Math.random() * 5))].map((_, index) => {
+        let start = add(startOfTheWeek, { days: Math.floor(Math.random() * 7), hours: 7 + Math.floor(Math.random() * 8) });
         return {
             start: start,
-            end: add(start, { minutes: (1 + Math.floor(Math.random() * 3)) * 30 }),
+            end: add(start, { minutes: (2 + Math.floor(Math.random() * 5)) * 30 }),
             title: `Event ${index + 1}`
         };
+    });
+
+    let overlapGroups: Array<Array<CalendarEntry>> = [];
+    for (let event of events) {
+        let overlapGroup = overlapGroups.find((group) =>
+            group.some((otherEvent) =>
+                dateIsBetween(event.start, otherEvent.start, otherEvent.end) || dateIsBetween(event.end, otherEvent.start, otherEvent.end)
+            )
+        );
+        if (overlapGroup) {
+            overlapGroup.push(event);
+        } else {
+            overlapGroups.push([event]);
+        }
+    }
+    return overlapGroups;
+});
+
+onMounted(() => {
+    nextTick(() => {
+        scrollViewportRef.value!.scrollTo({
+            top: dateToPercentOfDay(now.value) / 100 * scrollViewportRef.value!.scrollHeight - scrollViewportRef.value!.clientHeight + 150,
+        });
+        viewportIsReady.value = true;
     });
 });
 
@@ -52,16 +111,17 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
                     <!-- Placeholder for time -->
                 </div>
                 <div class="day" :class="{ 'today': day.isToday }" v-for="day in thisWeek" :key="day.date.getTime()">
-                    <span class="day-of-the-week">{{ day.dayOfTheWeek.substring(0, 1).toUpperCase() }}</span>
+                    <span class="day-of-the-week">{{ day.dayOfTheWeek.substring(0, 3).toUpperCase() }}</span>
                     <span class="day-of-the-month">{{ day.date.getDate() }}</span>
                 </div>
             </div>
         </div>
-        <div class="body">
-            <div class="days">
+        <div class="body" ref="scrollViewportRef">
+            <div class="days" :class="{ 'ready': viewportIsReady}">
                 <div class="time-indicators">
                     <div class="indicator" v-for="(hour, index) in 24" :key="hour"
                         :style="{ top: (100 * index / 24) + '%' }">
+
                         <div class="time">{{ (hour - 1).toString().padStart(2, '0') }}:00</div>
                         <div class="separator"></div>
                     </div>
@@ -70,23 +130,44 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
                 <div class="day" :class="{ 'first': index == 0 }" v-for="(day, index) in thisWeek"
                     :key="day.date.toString()">
                     <div class="events">
-                        <div class="event"
-                            v-for="event in calendarEntries.filter((el) => isSameDay(el.start, day.date))"
-                            :key="event.start.toString()"
-                            :style="{ top: dateToPercentOfDay(event.start) + '%', height: dateToPercentOfDay(new Date(event.end.getTime() - event.start.getTime())) + '%' }">
-                            <span>{{ event.title }}</span>
-                            <span class="time">{{ formatTime(event.start) }} – {{ formatTime(event.end) }}</span>
+                        <div class="event-overlap-group"
+                            v-for="group in calendarEventsWithOverlapGroups.filter((group) => group.some((event) => isSameDay(event.start, day.date)))"
+                            :key="group[0].start.toString()">
+                            <div class="event" v-for="(event, index) in group"
+                                :class="{ 'small': eventIsShort(event), isOverlaid: index >= 1 }"
+                                :key="event.start.toString()" :style="{
+                    top: dateToPercentOfDay(event.start) + '%',
+                    height: 'calc(' + eventDuration(event) * 100 / oneDayMs + '% - 1px)',
+                    left: index >= 1 ? '50%' : '0',
+                    paddingRight: index == 0 && group.length > 1 ? '50%' : '0'
+                }" :data-overlay-index="index" :data-overlay-group-size="group.length">
+                                <span>{{ event.title }}</span>
+                                <span v-if="!eventIsShort(event)" class="time">{{ formatTime(event.start) }} – {{
+                    formatTime(event.end) }}</span>
+                                <span v-else class="time">{{ formatTime(event.start) }}</span>
+                            </div>
+                        </div>
+                        <div v-if="day.isToday" class="now-indicator" :style="{ top: dateToPercentOfDay(now) + '%' }">
+                            <div class="dot"></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- dynamic days content -->
     </div>
 </template>
 
 <style lang="scss">
 .calendar {
+    --var-background: #ffffff;
+    --var-separator: #dfdfdf;
+
+    --var-accent: #1595e5;
+    --var-accent-hover: #128bc3;
+
+    --var-text: #333333;
+    --var-text-secondary: #7c7c7c;
+
     display: flex;
     flex-direction: column;
     width: 100%;
@@ -110,7 +191,7 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
             height: 100%;
             padding: 0 16px; // Same as body
 
-            font-size: 0.8rem;
+            color: var(--var-text-secondary);
 
             .day {
                 display: inline-flex;
@@ -118,8 +199,10 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
                 justify-content: center;
                 align-items: center;
                 flex: 1;
-                text-align: center;
                 gap: 6px;
+
+                text-align: center;
+                font-size: 0.7rem;
 
                 .day-of-the-month {
                     width: 32px;
@@ -128,7 +211,7 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
                     justify-content: center;
                     align-items: center;
 
-                    font-size: 1.2rem;
+                    font-size: 1.4rem;
                     border-radius: 50%;
 
                     &:hover {
@@ -139,8 +222,12 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
                 }
 
                 &.today .day-of-the-month {
-                    background-color: #17a9b3;
+                    background-color: var(--var-accent);
                     color: white;
+
+                    &:hover {
+                        background-color: var(--var-accent-hover);
+                    }
                 }
             }
 
@@ -155,7 +242,7 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
         height: 100%;
         overflow-y: scroll;
         overflow-x: hidden;
-        background-color: #f2f2f2;
+        background-color: var(--var-background);
 
         .days {
             display: flex;
@@ -164,6 +251,12 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
             height: 1500px;
             padding: 16px;
             position: relative;
+
+            opacity: 0;
+            &.ready {
+                opacity: 1;
+                transition: opacity 0.15s;
+            }
 
             .time-indicators {
                 position: absolute;
@@ -182,13 +275,16 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
                     justify-content: stretch;
                     align-items: center;
                     gap: 8px;
+                    font-size: 0.8rem;
+                    color: var(--var-text-secondary);
 
                     .separator {
                         width: 100%;
                         height: 1px;
-                        background-color: #a4a4a4;
+                        background-color: var(--var-separator);
                     }
                 }
+
             }
 
             .time-indicators-placeholder {
@@ -201,10 +297,10 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
                 position: relative;
 
                 display: block;
-                border-right: 1px solid #a4a4a4;
+                border-right: 1px solid var(--var-separator);
 
                 &.first {
-                    border-left: 1px solid #a4a4a4;
+                    border-left: 1px solid var(--var-separator);
                 }
 
                 .events {
@@ -212,16 +308,35 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
                     height: 100%;
                     position: absolute;
 
+                    .now-indicator {
+                        position: absolute;
+                        width: 100%;
+                        height: 2px;
+                        transform: translateY(-50%);
+                        background-color: #ec3737;
+
+                        .dot {
+                            position: absolute;
+                            width: 11px;
+                            height: 11px;
+                            background-color: #ec3737;
+                            border-radius: 50%;
+                            top: 50%;
+                            transform: translate(-50%, -50%);
+                        }
+                    }
+
                     .event {
                         flex: 1;
                         position: absolute;
                         left: 0;
                         right: 12px;
+                        overflow: hidden;
 
                         display: inline-flex;
                         flex-direction: column;
-                        justify-content: start;
-                        background: #1595e5;
+                        justify-content: flex-start;
+                        background: var(--var-accent);
                         color: white;
                         border-radius: 4px;
                         padding: 4px 8px;
@@ -229,7 +344,18 @@ const calendarEntries: globalThis.Ref<Array<CalendarEntry>> = useState('calendar
                         cursor: pointer;
 
                         &:hover {
-                            background: #0f8ac4;
+                            background: var(--var-accent-hover);
+                        }
+
+                        &.small {
+                            font-size: 0.7rem;
+                            flex-direction: row;
+                            gap: 8px;
+                        }
+
+                        &.isOverlaid {
+                            outline: 1px solid white;
+                            right: 0;
                         }
 
                         .time {
