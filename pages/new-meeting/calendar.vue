cfa6d7aedd3dae08358a8f617e334e2df8bd11ab
Filippo Orru <filippoorru@hotmail.com>
2024-03-13 19:24:51 +0100

    Calendar: small improvements

--- pages/new-meeting/calendar.vue
+++ pages/new-meeting/calendar.vue
@@ -60,12 +60,22 @@ function formatTime(date: Date) {
 }
 
 function formatTimeslotDuration(slot: CalendarTimeslot) {
+    let duration;
     if (draggingTimeslotPosition.value && draggingTimeslotPosition.value.slot == slot) {
         let draggingSlot = getTimeslotWithDrag(slot, draggingTimeslotPosition.value);
-        return formatDuration(intervalToDuration({ start: draggingSlot.start, end: draggingSlot.end }), { format: ['hours', 'minutes'], zero: true });
+        duration = intervalToDuration({ start: draggingSlot.start, end: draggingSlot.end });
     } else {
-        return formatDuration(intervalToDuration({ start: slot.start, end: slot.end }));
+        duration = intervalToDuration({ start: slot.start, end: slot.end });
     }
+
+    let str = [""];
+    if (duration.hours) {
+        str.push(duration.hours + "h");
+    }
+    if (duration.minutes) {
+        str.push(duration.minutes + "m");
+    }
+    return str.join(" ");
 }
 
 function showAddTimeslotButtonFor(day: Date, hour: number) {
@@ -201,6 +211,8 @@ function dragTimeslotMove(event: TouchEvent | MouseEvent, dragY?: number, isTouc
 }
 
 function dragTimeslotEnd(dragY?: number) {
+    longPressing = false;
+
     if (!draggingTimeslotPosition.value) return;
     let y = dragY || draggingTimeslotPosition.value.currentY;
 
@@ -210,7 +222,6 @@ function dragTimeslotEnd(dragY?: number) {
     draggingTimeslotPosition.value.slot.end = draggingSlot.end;
     mergeOverlappingTimeslots();
     draggingTimeslotPosition.value = null;
-    longPressing = false;
 }
 
 
@@ -292,7 +303,7 @@ onMounted(() => {
                             </div>
                         </div>
                         <span class="w-12 flex-shrink-0"></span>
-                        <div v-for="(day, index) in thisWeek" class="flex-1 h-full relative border-r border-gray-200"
+                        <div v-for="(day, index) in  thisWeek " class="flex-1 h-full relative border-r border-gray-200"
                             :class="{ 'border-l': index == 0 }" :key="day.date.toString()"
                             @mousemove="(event) => dragTimeslotMove(event, event.screenY)"
                             @mouseup="(event) => dragTimeslotEnd(event.screenY)"
@@ -341,29 +352,49 @@ onMounted(() => {
                                 </div>
 
                                 <!-- Time slots -->
-                                <div v-for="slot in store.selectedTimes.filter((slot) => isSameDay(slot.start, day.date))"
-                                    :key="slot.start.toString()" class="striped-background absolute left-0 right-1 md:right-2 inline-flex flex-col justify-start 
-    text-white rounded-md text-xs break-all
-    active:bg-blue-600/80" :style="{ top: getTimeslotTop(slot) + '%', height: 'calc(' + getTimeslotHeight(slot) * 100 + '% - 1px)', }"
-                                    @mousedown="(event) => dragTimeslotStart(event, slot, 'both', event.screenY)">
+                                <div v-for="slot in  store.selectedTimes.filter((slot) => isSameDay(slot.start, day.date)) "
+                                    :key="slot.start.toString()"
+                                    class="
+                                    bg-blue-500 absolute left-0 right-1 md:right-2 inline-flex flex-col justify-start text-white rounded-md text-xs break-all group"
+                                    :style="{
+        '-webkit-touch-callout': 'none', top: getTimeslotTop(slot) + '%', height: 'calc('
+            + getTimeslotHeight(slot) * 100 + '% - 1px)',
+    }" :class="{ 'striped-background': draggingTimeslotPosition?.slot !== slot || draggingTimeslotPosition?.which !== 'both' }">
                                     <!-- Time slot -->
-                                    <div class="relative h-full py-2 px-1 md:px-2 md:py-2 overflow-hidden ">
+                                    <div class="relative h-full py-2 px-1 md:px-2 md:py-2 overflow-hidden">
                                         <div class="absolute left-0 right-0 top-3 bottom-3"
-                                            @touchstart="(event) => dragTimeslotStart(event, slot, 'both', event.touches[0] && event.touches[0].screenY)">
+                                            @touchstart="(event) => dragTimeslotStart(event, slot, 'both', event.touches[0] && event.touches[0].screenY)"
+                                            @touchcancel="(event) => dragTimeslotEnd(event.touches[0] && event.touches[0].screenY)"
+                                            @mousedown="(event) => dragTimeslotStart(event, slot, 'both', event.screenY)">
                                             <!-- Center drag area for touch -->
                                         </div>
 
-                                        <span class="transition-all" :class="{ 'opacity-0': isShort(slot) || draggingTimeslotPosition?.slot === slot }">
+                                        <span class="transition-all"
+                                            :class="{ 'opacity-0': isShort(slot) || draggingTimeslotPosition?.slot === slot && draggingTimeslotPosition?.which !== 'both' }">
                                             {{ formatTimeslotDuration(slot) }}</span>
                                     </div>
                                     <!-- Drag handles -->
-                                    <div class="absolute h-1 bg-white outline outline-4 cursor-pointer md:outline-2 outline-blue-400 rounded-full p-1 top-0 left-2 md:right-2 -translate-y-1/2 touch-none"
+                                    <div class="absolute bg-white/90 outline outline-4 cursor-pointer md:outline-2 outline-blue-400 rounded-full p-1 top-0 left-1 md:right-1 -translate-y-1/2
+                                    md:translate-y-0"
                                         @mousedown="(event) => dragTimeslotStart(event, slot, 'start', event.screenY)"
                                         @touchstart="(event) => dragTimeslotStart(event, slot, 'start', event.touches[0] && event.touches[0].screenY)">
+                                        <!-- Hover indicator -->
+                                        <div class="hidden absolute left-0 right-0 text-slate-500 -translate-y-1/2 md:flex justify-center 
+                                        opacity-0 transition-opacity group-hover:opacity-100"
+                                            :class="{ 'opacity-100': draggingTimeslotPosition?.slot === slot }">
+                                            <font-awesome-icon icon="caret-up" />
+                                        </div>
                                     </div>
-                                    <div class="absolute 1 bg-white outline outline-4 cursor-pointer md:outline-2 outline-blue-400 rounded-full p-1 bottom-0 right-2 md:left-2 translate-y-1/2 touch-none"
+                                    <div class="absolute bg-white/90 outline outline-4 cursor-pointer md:outline-2 outline-blue-400 rounded-full p-1 bottom-0 right-1 md:left-1 translate-y-1/2
+                                    md:translate-y-0"
                                         @mousedown="(event) => dragTimeslotStart(event, slot, 'end', event.screenY)"
                                         @touchstart="(event) => dragTimeslotStart(event, slot, 'end', event.touches[0] && event.touches[0].screenY)">
+                                        <!-- Hover indicator -->
+                                        <div class="hidden absolute left-0 right-0 text-slate-500 -translate-y-1/2 md:flex justify-center 
+                                    opacity-0 transition-opacity group-hover:opacity-100"
+                                            :class="{ 'opacity-100': draggingTimeslotPosition?.slot === slot }">
+                                            <font-awesome-icon icon="caret-down" />
+                                        </div>
                                     </div>
                                 </div>
 
@@ -392,9 +423,9 @@ onMounted(() => {
                     @click="bottomSheetExpanded = false"></div>
 
                 <!-- Sidebar -->
-                <div class="w-full absolute bottom-0 px-3 py-8 pt-0 bg-white border-t rounded-tr-2xl rounded-tl-2xl overflow-hidden
+                <div class="w-full absolute bottom-0 px-3 md:py-8 bg-white border-t rounded-tr-2xl rounded-tl-2xl flex flex-col overflow-hidden
                 h-16 data-[expanded=true]:h-[80%] md:data-[expanded=true]:h-full
-                 md:overflow-y-auto md:h-full md:block md:border-0 md:border-r md:rounded-none md:pt-8 pointer-events-auto
+                 md:h-full md:border-0 md:border-r md:rounded-none pointer-events-auto
                     shadow-[0_-5px_11px_0_rgba(0,0,0,0.1)] z-10 transition-all" :data-expanded="bottomSheetExpanded">
 
                     <!-- Sidebar head -->
@@ -404,25 +435,28 @@ onMounted(() => {
                         <p class="text-xl text-slate-600">Your Event</p>
                     </div>
 
-                    <div class="h-full overflow-auto md:overflow-hidden">
-                        <label class="text-gray-500 mt-4">Title</label>
-                        <input type="text" v-model="store.eventTitle" name="title" maxlength="100"
-                            class="bg-gray-50 border border-gray-400 text-gray-800 text-sm rounded-lg focus:ring-blue-200 block w-full p-2.5"
-                            placeholder="Event title (optional)" :required="false" />
+                    <div class="h-full overflow-auto">
+                        <div class=" pb-8">
+                            <label class="text-gray-500 mt-4">Title</label>
+                            <input type="text" v-model="store.eventTitle" name="title" maxlength="100"
+                                class="bg-gray-50 border border-gray-400 text-gray-800 text-sm rounded-lg focus:ring-blue-200 block w-full p-2.5"
+                                placeholder="Event title (optional)" :required="false" />
 
-                        <!-- Timeslots by day -->
-                        <p class="text-gray-500 mt-4">Selected times</p>
-                        <div class="flex flex-col gap-2 mt-2 text-sm">
-                            <p v-if="store.selectedTimes.length == 0" class="text-gray-500">No times selected yet</p>
-                            <div v-for="slots in store.selectedTimesByDay"
-                                class="flex flex-col gap-2 bg-gray-100 py-2 rounded-md">
-                                <p class="text-slate-600 px-2">{{ formatSelectedTimesDay(slots.day) }}</p>
-                                <div class="text-slate-500 flex justify-between pr-2 hover:bg-slate-500/10 px-2"
-                                    v-for="slot in slots.timeslots">
-                                    <span>{{ formatSelectedTimeslotTime(slot) }}</span>
-                                    <button class="rounded-full hover:bg-slate-500/20 h-6 w-6"
-                                        @click="() => removeTimeslot(slots.day, slot)"><font-awesome-icon
-                                            icon="times" /></button>
+                            <!-- Timeslots by day -->
+                            <p class="text-gray-500 mt-4">Selected times</p>
+                            <div class="flex flex-col gap-2 mt-2 text-sm">
+                                <p v-if="store.selectedTimes.length == 0" class="text-gray-500">No times selected yet
+                                </p>
+                                <div v-for=" slots  in  store.selectedTimesByDay "
+                                    class="flex flex-col gap-2 bg-gray-100 py-2 rounded-md">
+                                    <p class="text-slate-600 px-2">{{ formatSelectedTimesDay(slots.day) }}</p>
+                                    <div class="text-slate-500 flex justify-between pr-2 hover:bg-slate-500/10 px-2"
+                                        v-for=" slot  in  slots.timeslots ">
+                                        <span>{{ formatTime(slot.start) }} – {{ formatTime(slot.end) }}</span>
+                                        <button class="rounded-full hover:bg-slate-500/20 h-6 w-6"
+                                            @click="() => removeTimeslot(slots.day, slot)"><font-awesome-icon
+                                                icon="times" /></button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -436,6 +470,6 @@ onMounted(() => {
 
 <style>
 .striped-background {
-    background: repeating-linear-gradient(45deg, #2585e9ba, #2585e9ba 10px, #2585e9 10px, #2585e9 20px);
+    background: repeating-linear-gradient(-225deg, #2585e9ba, #2585e9ba 10px, #2585e9 10px, #2585e9 20px);
 }
 </style>
