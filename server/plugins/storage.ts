dac7af47e4ef72b5c7633aa5bb8492131813ac69
Filippo Orru <filippoorru@hotmail.com>
2024-03-17 23:26:27 +0100

    i'm dying

--- server/plugins/storage.ts
+++ server/plugins/storage.ts
@@ -1,16 +1,16 @@
 import redisDriver from 'unstorage/drivers/redis'
 
-export default defineNuxtPlugin(() => {
-  const storage = useStorage()
+export default defineNitroPlugin(() => {
+  const storage = useStorage();
 
   // Dynamically pass in credentials from runtime configuration, or other sources
   const driver = redisDriver({
-      base: 'redis',
-      host: useRuntimeConfig().redis.host,
-      port: 6379,
-      /* other redis connector options */
-    })
+    base: 'redis',
+    host: useRuntimeConfig().redis.host,
+    port: 6379,
+    /* other redis connector options */
+  })
 
   // Mount driver
   storage.mount('redis', driver)
-})
+});
