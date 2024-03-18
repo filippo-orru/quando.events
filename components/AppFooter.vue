c7e2530553f652ba89edc1da62ff631f227ca8de
Filippo Orru <filippoorru@hotmail.com>
2024-03-18 10:55:40 +0100

    Fix footer on small devices

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,12 +3,12 @@
 <template>
     <footer class="bg-secondary-800 text-secondary-100 pt-8 pb-24 mt-36">
         <div
-            class="container max-w-screen-lg px-12 mx-auto flex justify-between items-center [&_a:hover]:text-secondary">
-            <span class="tracking-wide">
+            class="container max-w-screen-lg px-12 mx-auto flex flex-col md:flex-row gap-8 justify-center md:justify-between items-center [&_a:hover]:text-secondary">
+            <span class="flex-0 tracking-wide">
                 Made with <font-awesome-icon :icon="['fas', 'heart']" class="heartbeat mx-1"/> by
                 <a href="https://filippo-orru.com" class="underline">Filippo Orru</a>
             </span>
-            <nav class="flex gap-8 [&_a:hover]:underline">
+            <nav class="flex-0 flex gap-8 [&_a:hover]:underline">
                 <a href="#" class="">Privacy</a>
                 <a href="#" class="">Terms</a>
             </nav>
