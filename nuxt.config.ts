05c6ca21ced96e4127333b409935dba47f8fbd68
Filippo Orru <filippoorru@hotmail.com>
2024-03-31 11:55:19 +0200

    Set html lang

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
-      'link': [
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
